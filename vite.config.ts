281366bc6add6b4c6bf058b781860a2bada9c4ad
MiyamizuSu <14892138+miyamizusu@user.noreply.gitee.com>
2025-06-19 16:32:53 +0800

    build@3 plugin

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,20 @@ export default defineConfig({
     alias:{
       '@':path.resolve(__dirname,'./src')
     }
+  },
+  build:{
+    rollupOptions:{
+      output:{
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // 拆成 node_modules/包名，例如 node_modules/lodash-es/
+            const parts = id.toString().split('node_modules/')[1].split('/');
+            const name = parts[0].startsWith('@') ? `${parts[0]}/${parts[1]}` : parts[0];
+            return `npm.${name}`;
+          }
+        }
+      }
+    }
   }
 })
 
