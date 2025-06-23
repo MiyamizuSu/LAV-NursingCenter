dcd1226593f590215995fb51bc838247823e7da2
MiyamizuSu <14892138+miyamizusu@user.noreply.gitee.com>
2025-06-23 15:54:56 +0800

    buildStore@1

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,17 +1,20 @@
-import { ref } from "vue"
-
-const appState=ref<Set<string>>(new Set<string>())
-export function useAppState(){
-    return {
-        get:function(){
-            return appState.value
-        },
-        addState:function(memoryName:string){
-          appState.value.add(memoryName)   
-        },
-        deleteState:function(memoryName:string){
-            appState.value.delete(memoryName)
-        },
-        appState:appState
-    }
+import { ref } from "vue";
+import { defineStore } from "pinia";
+const appState = ref<Set<string>>(new Set<string>());
+export function useAppState() {
+  return {
+    get: function () {
+      return appState.value;
+    },
+    addState: function (memoryName: string) {
+      appState.value.add(memoryName);
+    },
+    deleteState: function (memoryName: string) {
+      appState.value.delete(memoryName);
+    },
+    appState: appState,
+  };
 }
+export const useBedLayoutStore = defineStore("bedStore", ()=>{
+    
+});
