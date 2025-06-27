93cc2f93dafee6badfbcee269c3ccf3c501268de
Yrugit <577054257@qq.com>
2025-06-27 21:57:26 +0800

    package rem

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import { router } from './lib/router'
+// import 'amfe-flexible'
+import { setDomFontSize } from './lib/utils'
 
+// setDomFontSize()
 createApp(App).use(router).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,37 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import postCssPxToRem from 'postcss-pxtorem'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),tailwindcss(),vueDevTools(),],
+  plugins: [
+    vue(),tailwindcss(),vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve:{
     alias:{
       '@':path.resolve(__dirname,'./src')
     }
-  }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        postCssPxToRem({
+          rootValue: 16, // 基准值
+          propList: ['*'], // 需要转换的属性，这里选择全部
+          selectorBlackList: [], // 过滤掉不需要转换的类名
+        }),
+      ],
+    },
+  },
 })
 
