c91e68aac4af3bd19a19816db0529616d27b6b8d
suyeguanxing <suyeguanxing@qq.com>
2025-06-28 18:32:13 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
 import 'vue-sonner/style.css'
-
+import CheckOutPage from './pages/ctm/CheckOutPage.vue';
+import SidebarProvider from './components/ui/sidebar/SidebarProvider.vue';
+import AppSideBar from './components/custom/AppSideBar.vue';
+import SidebarTrigger from './components/ui/sidebar/SidebarTrigger.vue';
+import Button from './components/ui/button/Button.vue';
+import CheckInPage from './pages/ctm/CheckInPage.vue';
+import GoOutPage from './pages/ctm/GoOutPage.vue';
+import GoOutApplication from './pages/cts/GoOutApplication.vue';
+import CheckOutApplication from './pages/cts/CheckOutApplication.vue';
 const router=useRouter()
 router.push('/login')
 
@@ -16,7 +24,11 @@ router.push('/login')
         <Button>
         </Button>
       </SidebarTrigger>
-      <CheckOut></CheckOut>
+      <!-- <CheckInPage></CheckInPage> -->
+      <!-- <CheckOutPage></CheckOutPage> -->
+      <!-- <GoOutPage></GoOutPage> -->
+      <GoOutApplication></GoOutApplication>
+      <!-- <CheckOutApplication></CheckOutApplication> -->
     </SidebarProvider>
   </div>
 </template>
